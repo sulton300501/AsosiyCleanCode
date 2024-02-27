23b4acc7804a7ecfaa81dc871a42146cc48f40a7
sulton <sultonmalik326@gmail.com>
2024-02-27 17:57:23 +0500

    Add project files.

---
+++ FileLesson.API/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using FileApiLesson.Application.Service.UserProfilService;
+using FileAPILesson.Domain.Entities.DTOs;
+using FileLesson.API.ExternalService;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FileLesson.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly IWebHostEnvironment _env;
+        private readonly IUserProfilService _userService;
+
+        public UserController(IUserProfilService userProfilService, IWebHostEnvironment env)
+        {
+            _userService = userProfilService;
+            _env= env;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromForm] UserProfileDTO userProfileDTO)
+        {
+            UserProfileExternalService service = new UserProfileExternalService(_env);
+
+            var picture = userProfileDTO.Picture;
+            
+            var path = service.AddPictureAndGetPath(picture);
+
+            string picturePath = await service.AddPictureAndGetPath(picture);
+
+
+            var result = await _userService.CreateUserProfileAsync(userProfileDTO, picturePath);
+
+
+            return Ok(result);
+        }
+
+      
+        [HttpPost]
+        public IActionResult PostForm([FromForm] string value ,IFormFile file)
+        {
+            return Ok(value);
+        }
+
+      
+    
+    }
+}
---
+++ FileLesson.API/ExternalService/UserProfileExternalService.cs
@@ -0,0 +1,29 @@
+﻿namespace FileLesson.API.ExternalService
+{
+    public  class UserProfileExternalService
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public UserProfileExternalService(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+      
+
+
+        public  async Task<string> AddPictureAndGetPath(IFormFile file)
+        {
+            string path = Path.Combine(_env.WebRootPath, "images", Guid.NewGuid().ToString());//windows. c  windows\c   windows/c
+
+            Directory.CreateDirectory(path);
+
+            using(var stream = File.Create(Path.Combine(path, file.FileName)))
+            {
+               await  file.CopyToAsync(stream);
+            }
+
+            return path;
+        
+        }
+    }
+}
